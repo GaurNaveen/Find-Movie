a11bbac472f561ee154a6aa3adea2ac42ceadaa5
Naveen Gaur <naveengaur800@gmail.com>
2019-08-29 16:22:14 +1000

    Basic implementation of the Moya Framework has been done.

---
+++ Find Movie/APICredentialsFile.swift
@@ -0,0 +1,11 @@
+//
+//  APICredentialsFile.swift
+//  Find Movie
+//
+//  Created by Naveen Gaur on 29/8/19.
+//  Copyright © 2019 Naveen Gaur. All rights reserved.
+//
+
+import Foundation
+
+
--- Find Movie/Service/Service.swift
+++ Find Movie/Service/Service.swift
@@ -18,8 +18,56 @@
 
 
 import UIKit
+import Moya
 
-class Service: NSObject {
-    
+enum MovieService {
+    case searchMovie(query: String)
 }
 
+    // MARK: - Variables
+    private let baseUrl = "https://api.themoviedb.org"
+    private let region = "us"
+    private let page = "1"
+    private let adult = "false"
+    private let language = "en-US"
+
+
+extension MovieService: TargetType {
+
+    var baseURL: URL {
+        guard let url = URL(string: baseUrl) else {fatalError("This String is not of URL type.")}
+        return url
+    }
+    
+    var path: String {
+        switch self {
+        case .searchMovie:
+            return "/3/search/movie"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .searchMovie:
+            return .get
+        }
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        switch self {
+        case .searchMovie(let query):
+            return .requestParameters(parameters: ["query":query,"language":language,
+                                                   "page":page,"include_adult":adult,
+                                                    "region":region], encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["X-Api-Key":""]
+    }
+    
+}
