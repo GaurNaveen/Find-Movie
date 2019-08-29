9e9e453415df3de636f69641b2f9da5b7f5971b8
Naveen Gaur <naveengaur800@gmail.com>
2019-08-29 13:13:18 +1000

    Initial Commit

---
+++ Find Movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Find Movie
+//
+//  Created by Naveen Gaur on 29/8/19.
+//  Copyright © 2019 Naveen Gaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Find MovieTests/Find_MovieTests.swift
@@ -0,0 +1,34 @@
+//
+//  Find_MovieTests.swift
+//  Find MovieTests
+//
+//  Created by Naveen Gaur on 29/8/19.
+//  Copyright © 2019 Naveen Gaur. All rights reserved.
+//
+
+import XCTest
+@testable import Find_Movie
+
+class Find_MovieTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
